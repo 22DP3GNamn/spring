41df50483cd7b8014d1adb42a1e9ebf446f8cd9e
Gvido_n <131261294+22DP3GNamn@users.noreply.github.com>
2023-12-12 12:54:29 +0000

    kaut kas jauns iesaakts tad jau redzes

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
@@ -11,5 +12,23 @@ public class DefaultController {
    @GetMapping(value = "/")
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/hello")
+    String hello(){
+        return "hello";
+    } 
+@GetMapping(value = "/about")
+ModelAndView about(){
+    ModelAndView modelAndView = new ModelAndView("about");
+    return modelAndView;
+    }
+    @GetMapping(value = "/Links")
+    String Links(){
+        return "Links";
+    }
+    @GetMapping(value = "/Experiment")
+    String Experiment(){
+        return "Experiment";
     }
 }
