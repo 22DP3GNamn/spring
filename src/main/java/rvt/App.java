a55110e27cb476fe523f02a7b41ad17fa4cd16bb
Gvido_n <131261294+22DP3GNamn@users.noreply.github.com>
2024-01-17 07:53:05 +0000

    macins

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -8,4 +8,14 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+    public static void Money(){
+        Money macins1 = new Money(0, 2);
+        Money macins2 = new Money(0, 3);
+        Money kopejais_macins = macins1.plus(macins2);
+        System.out.println();
+        System.out.println("Kopā abos maciņos ir " + kopejais_macins + " eiro");
+        System.out.println("Pirmajā maciņā ir vairāk nekā otrajā maciņā: "+ macins1.less(macins2));
+        Money Starp_macins = macins1.minus(macins2);
+        System.out.println("Atņemot no pirmā maciņa otro, paliek " + Starp_macins + " eiro");
+}
 }
---
+++ src/main/java/rvt/Money.java
@@ -0,0 +1,62 @@
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents < 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros; 
+        int newCents = this.cents + addition.cents;
+        if(newCents >= 100){
+            newEuros += 1;
+            newCents -=100;
+        }
+        Money newMoney = new Money(newEuros, newCents);
+        return newMoney;
+    }
+    public boolean less(Money less) {
+        int newEuro = this.euros - less.euros;
+        int newCents = this.cents - less.cents;
+        if (newEuro< 0 && newCents< 0 || newEuro==0 && newCents< 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+    public Money minus(Money minus) {
+        int newEuro = this.euros - minus.euros;
+        int newCents = this.cents - minus.cents;
+        if(newCents < 0){
+            newEuro -= 1;
+            newCents += 100;
+        }
+        if (newEuro < 0){
+            newEuro = 0;
+            newCents = 0;
+        }
+        Money newMoney = new Money(newEuro, newCents);
+        return newMoney;
+    }
+}
